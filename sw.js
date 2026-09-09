833b2a7fbd6be5cea45e39e60e991af2588a1976
nk7fbvg4zf-ship-it <nk7fbvg4zf@privaterelay.appleid.com>
2026-09-10 01:48:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="hyena-v2-20260910";
+const C="hyena-v3-20260910";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{let z=r.clone();caches.open(C).then(c=>c.put(e.request,z));return r}).catch(()=>caches.match(e.request))));
