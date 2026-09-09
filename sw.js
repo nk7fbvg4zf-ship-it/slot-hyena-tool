5035752d739409c1ff054ae6b3d700257aa2bfe4
nk7fbvg4zf-ship-it <nk7fbvg4zf@privaterelay.appleid.com>
2026-09-10 02:42:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil((async()=>{
-  const ks=await caches.keys();
-  await Promise.all(ks.map(k=>caches.delete(k)));
-  await self.registration.unregister();
-  const cs=await self.clients.matchAll({type:'window'});
-  cs.forEach(c=>c.navigate(c.url));
+ const ks=await caches.keys(); await Promise.all(ks.map(k=>caches.delete(k)));
+ await self.registration.unregister();
 })()));
 self.addEventListener('fetch',()=>{});
